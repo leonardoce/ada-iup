a5368f761b5b769b834df79afdbdf2c7fa9ac7bc
Leonardo Cecchi <leonardoce@interfree.it>
2014-04-21 18:36:30 +0200

    working

--- iup-util.ads
+++ iup-util.ads
@@ -1,4 +1,8 @@
 package Iup.Util is
+    -- -----------------------
+    -- Widget helper functions
+    -- -----------------------
+
     -- Function: Grid_Box
     -- Create a Grid Box with the specified number of divisions
     function Grid_Box(Num_Div:Positive; Children:Handle_Array) return Handle;
